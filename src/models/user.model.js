ab539b47aa5e252473d401898104320961a9a011
Gbaboh Ernest <gbaboh.ernest@gmail.com>
2025-04-28 08:24:33 +0300

    Add pre save hooks for hashing user password; Add instance methods to generate jwt and verify user hashed password

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,7 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { JWT_EXPIRES_IN, JWT_SECRET_KEY } from "../../config/env.js";
 
 const userSchema = new mongoose.Schema(
   {
@@ -10,7 +13,7 @@ const userSchema = new mongoose.Schema(
       maxLength: [30, "Username cannot exceeds 30 characters"],
       match: [
         /^[A-Za-z][A-Za-z0-9_]{2,29}$/,
-        "Username must start with a letter and only contain letters, numbers, and underscores",
+        "Username must start with a letter and only contain letters, numbers, and underscores, no space(s)",
       ],
       index: true,
     },
@@ -33,7 +36,6 @@ const userSchema = new mongoose.Schema(
       required: [true, "Password is required"],
       minLength: [8, "Must be at least 8 characters"],
       maxLength: [128, "Password cannot exceed 128 character"],
-      select: false,
     },
 
     role: {
@@ -48,6 +50,25 @@ const userSchema = new mongoose.Schema(
   { timestamps: true },
 );
 
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+
+  next();
+});
+
+userSchema.methods.isPwdVerified = async function (userPassword) {
+  return await bcrypt.compare(userPassword, this.password);
+};
+
+userSchema.methods.generateToken = async function () {
+  return jwt.sign(
+    { userId: this._id, username: this.username, role: this.role },
+    JWT_SECRET_KEY,
+    { expiresIn: JWT_EXPIRES_IN },
+  );
+};
 const User = mongoose.model("User", userSchema);
 
 export default User;
