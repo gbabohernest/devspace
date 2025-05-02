620d3ca3fb03e085e4bbb766c0f37af4f75d7391
Gbaboh Ernest <gbaboh.ernest@gmail.com>
2025-05-02 17:32:58 +0300

    Add option to filter resource based on search terms when fetching paginated list of resource

--- src/utils/paginate.js
+++ src/utils/paginate.js
@@ -1,20 +1,48 @@
 /**
- * A helper function for filtering, sorting and paginating data
+ * A helper function for filtering, searching,  sorting and paginating data
  *
  * @param req - Request object
  * @param Model - A mongoose data model
- * @param filter - Query filter
+ * @param baseFilter - Query filter
  * @param sort - Sort query
  * @param virtual - Document's Virtual field
+ * @param searchableFields - An array of  search fields.
  * @returns Object - Containing paginated data
  */
 
-const paginate = async (req, Model, filter = {}, sort = {}, virtual = "") => {
+const paginate = async (
+  req,
+  Model,
+  baseFilter = {},
+  sort = {},
+  virtual = "",
+  searchableFields = [],
+) => {
   //prevent negative page/limit values
   const page = Math.max(1, parseInt(req.query?.page)) || 1;
-  const limit = Math.min(50, Math.max(1, parseInt(req.query?.limit))) || 2;
+  const limit = Math.min(50, Math.max(1, parseInt(req.query?.limit))) || 5;
 
   const skip = (page - 1) * limit;
+
+  let filter = { ...baseFilter };
+
+  if (req.query.search && searchableFields.length > 0) {
+    const searchRegex = { $regex: req.query.search, $options: "i" };
+
+    filter.$or = searchableFields.map((field) => {
+      return { [field]: searchRegex };
+    });
+
+    /*
+    let filterEx = {
+      isPublic: true,
+      $or: [
+        { title: { $regex: "portfolio", $options: "i" } },
+        { tech: { $regex: "portfolio", $options: "i" } },
+      ],
+    };
+     */
+  }
 
   const [data, totalItems] = await Promise.all([
     Model.find(filter)
@@ -28,7 +56,7 @@ const paginate = async (req, Model, filter = {}, sort = {}, virtual = "") => {
   const totalPages = Math.ceil(totalItems / limit);
 
   return {
-    nbHits: totalItems,
+    nbHits: data.length,
     currentPage: page,
     numberOfPages: totalPages,
     limit,
