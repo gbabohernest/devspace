4ce75c0afd93f8cff9c314d020921cd83ec01832
Gbaboh Ernest <gbaboh.ernest@gmail.com>
2025-05-02 21:12:40 +0300

    Modified the findById query, use a paginate function to fetch paginated list of user's project, leveraging sorting, seaching and filtering of the data

--- src/controllers/users/me.controller.js
+++ src/controllers/users/me.controller.js
@@ -3,6 +3,7 @@ import { ResourceNotFoundError } from "../../utils/index.utils.js";
 import formatDate from "../../utils/helpers/dateFormatter.js";
 import Project from "../../models/project.model.js";
 import { StatusCodes } from "http-status-codes";
+import paginate from "../../utils/paginate.js";
 
 /**
  * Get User information
@@ -11,13 +12,9 @@ import { StatusCodes } from "http-status-codes";
  * @returns {Promise<ResourceNotFoundError>}
  */
 const getThisUserInfo = async (req, res) => {
-  const { userId, username: name } = req.userInfo;
+  const { userId } = req.userInfo;
 
-  const user = await User.findOne(
-    { _id: userId, username: name },
-    "-password -_id",
-    null,
-  );
+  const user = await User.findById(userId, "-password -_id", null);
 
   if (!user) {
     return new ResourceNotFoundError("No user found");
@@ -32,7 +29,9 @@ const getThisUserInfo = async (req, res) => {
     Joined: formatDate(createdAt),
   };
 
-  res.status(200).json({ success: true, message: "Your info sir", userData });
+  res
+    .status(200)
+    .json({ success: true, message: "Your Data is Retrieved", userData });
 };
 
 /**
@@ -44,23 +43,26 @@ const getThisUserInfo = async (req, res) => {
 const getAllThisUserProjects = async (req, res) => {
   const { userId } = req.userInfo;
 
-  const projects = await Project.find({ createdBy: userId }, "", null).sort(
-    "updatedAt-",
+  const projects = await paginate(
+    req,
+    Project,
+    { createdBy: userId },
+    "",
+    { updatedAt: -1 },
+    ["title", "description", "tech"],
   );
 
-  if (!projects || projects.length < 1) {
+  if (projects.data.length < 1) {
     return res.status(StatusCodes.OK).json({
       success: true,
-      nbHits: `${projects.length}`,
       message: `You have not created any Project(s). Start creating project(s)`,
-      projects,
+      projects: projects.data,
     });
   }
 
   res.status(200).json({
     success: true,
     message: "A paginated list of all your project(s)",
-    nbHits: projects.length,
     projects,
   });
 };
