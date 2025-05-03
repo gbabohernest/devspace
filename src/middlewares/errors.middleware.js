d810cdb89261bd08f08860b2eda57b6914382221
Gbaboh Ernest <gbaboh.ernest@gmail.com>
2025-05-03 19:49:48 +0300

    Check for mongoose duplicate key and cast error

--- src/middlewares/errors.middleware.js
+++ src/middlewares/errors.middleware.js
@@ -1,10 +1,20 @@
 import { StatusCodes, getReasonPhrase } from "http-status-codes";
 
-const { INTERNAL_SERVER_ERROR, BAD_REQUEST } = StatusCodes;
+const { INTERNAL_SERVER_ERROR, BAD_REQUEST, CONFLICT } = StatusCodes;
 
 const errorMiddleware = (err, req, res, next) => {
   let message = err.message || getReasonPhrase(INTERNAL_SERVER_ERROR);
   let statusCode = err.statusCode || INTERNAL_SERVER_ERROR;
+
+  if (err.code === 11000) {
+    message = "Duplicate Key Error Collection [ value(s) already exists ]";
+    statusCode = CONFLICT;
+  }
+
+  if (err?.name === "CastError") {
+    statusCode = BAD_REQUEST;
+    message = "Please enter a valid resource id";
+  }
 
   if (err?.name === "ValidationError") {
     if (err?.errors) {
