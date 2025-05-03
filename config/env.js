881d2022866bff188f5782b52426786f9a04815a
Gbaboh Ernest <gbaboh.ernest@gmail.com>
2025-05-03 06:12:41 +0300

    DevSpace configs

--- config/env.js
+++ config/env.js
@@ -2,4 +2,15 @@ import { config } from "dotenv";
 
 config();
 
-export const { PORT, MONGO_URL } = process.env;
+export const {
+  PORT,
+  MONGO_URL,
+  NODE_ENV,
+  JWT_SECRET_KEY,
+  JWT_EXPIRES_IN,
+  DEFAULT_AVATAR_URL,
+  DEFAULT_AVATAR_PUBLIC_ID,
+  CLOUDINARY_CLOUD_NAME,
+  CLOUDINARY_API_KEY,
+  CLOUDINARY_API_SECRET,
+} = process.env;
