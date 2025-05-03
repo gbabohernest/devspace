e0e0d99116355b433c6dd35d7ef4ad2820a1bdc9
Gbaboh Ernest <gbaboh.ernest@gmail.com>
2025-05-03 19:51:05 +0300

    upload avatar route

--- src/routes/me.route.js
+++ src/routes/me.route.js
@@ -1,13 +1,21 @@
 import { Router } from "express";
 import authMiddleware from "../middlewares/authentication/auth.middleware.js";
 import {
+  changeAvatar,
   getAllThisUserProjects,
   getThisUserInfo,
 } from "../controllers/users/me.controller.js";
+import multerUploadMiddleware from "../middlewares/multer/multer.middleware.js";
 
 const meRouter = new Router();
 
 meRouter.get("/", authMiddleware, getThisUserInfo);
 meRouter.get("/projects", authMiddleware, getAllThisUserProjects);
+meRouter.post(
+  "/change-avatar",
+  authMiddleware,
+  multerUploadMiddleware.single("image"),
+  changeAvatar,
+);
 
 export default meRouter;
