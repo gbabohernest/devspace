da3bcd31ea3c146a53f70408afde93d87e46f29f
Gbaboh Ernest <gbaboh.ernest@gmail.com>
2025-05-05 23:31:43 +0300

    Add error message

--- src/utils/rate-limit.js
+++ src/utils/rate-limit.js
@@ -3,6 +3,7 @@ import { rateLimit } from "express-rate-limit";
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  message: "Too many requests, please try again later.",
 });
 
 export default limiter;
